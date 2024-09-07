2870ed0e67a7b91129fe03cec8562b0a97614556
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-09-07 18:56:20 +0900

    [Feat] 비밀번호 암호화 시 사용되는 PasswordEncoder 빈등록

---
+++ fridaymarket_auth/src/main/java/com/smile/fridaymarket_auth/global/config/WebSecurityConfig.java
@@ -0,0 +1,49 @@
+package com.smile.fridaymarket_auth.global.config;
+
+import com.smile.fridaymarket_auth.domain.auth.JwtAuthenticationFilter;
+import com.smile.fridaymarket_auth.domain.auth.token.TokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+
+        return new BCryptPasswordEncoder();
+    }
+
+    private final TokenProvider tokenProvider;
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf((AbstractHttpConfigurer::disable))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, ("/api/users/**")).permitAll()
+                        .requestMatchers("/swagger-ui/**",
+                                "/swagger-ui.html").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(new JwtAuthenticationFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+}
