8688f43a383d8a3485f257352bcaabe01eaf491a
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-09-07 18:53:29 +0900

    [Feat] User Entity 생성

---
+++ fridaymarket_auth/src/main/java/com/smile/fridaymarket_auth/domain/user/entity/User.java
@@ -0,0 +1,44 @@
+package com.smile.fridaymarket_auth.domain.user.entity;
+
+import com.smile.fridaymarket_auth.global.entity.Timestamped;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_USER")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "USER_ID", columnDefinition = "UUID")
+    private UUID id;
+
+    @Column(name = "USERNAME", length = 20, nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "PASSWORD", nullable = false)
+    private String password;
+
+    @Column(name = "PHONE_NUMBER", length = 20, nullable = false)
+    private String phoneNumber;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "USER_ROLE", length = 20)
+    private Set<UserRole> userRole = new HashSet<>();
+
+    @Column(name = "IS_DELETED", nullable = false)
+    private Boolean isDeleted = false;
+
+}
