bed01709ec8b39e3eea12c1e7c767e870eda109c
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-09-08 15:41:38 +0900

    [Refactor] 사용되지 않는 import 제거

--- fridaymarket_auth/src/test/java/com/smile/fridaymarket_auth/domain/user/service/UserServiceImplTest.java
+++ fridaymarket_auth/src/test/java/com/smile/fridaymarket_auth/domain/user/service/UserServiceImplTest.java
@@ -13,7 +13,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -21,10 +20,6 @@ import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @ActiveProfiles("test")
