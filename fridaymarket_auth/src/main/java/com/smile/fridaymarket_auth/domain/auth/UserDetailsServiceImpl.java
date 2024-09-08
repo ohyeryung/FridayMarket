cec9c6afdd10db903820a1e2ec4e07828ae4fc25
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-09-08 14:14:08 +0900

    [Feat] 토큰 검증 로직 구현 및 TokenProvider 클래스명 변경

---
+++ fridaymarket_auth/src/main/java/com/smile/fridaymarket_auth/domain/auth/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.smile.fridaymarket_auth.domain.auth;
+
+
+import com.smile.fridaymarket_auth.domain.user.entity.User;
+import com.smile.fridaymarket_auth.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl {
+
+    private final UserRepository userRepository;
+
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("가입되지 않은 아이디입니다."));
+
+        return new UserDetailsImpl(user);
+    }
+
+}
