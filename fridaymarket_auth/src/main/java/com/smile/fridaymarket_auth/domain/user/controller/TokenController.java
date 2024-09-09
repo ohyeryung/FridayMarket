d7b11eda0c9ef1a429cc40409d6d4e6cd4172ef7
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-09-10 00:52:07 +0900

    [Refactor] accessToken 재발급 API swagger 설정

--- fridaymarket_auth/src/main/java/com/smile/fridaymarket_auth/domain/user/controller/TokenController.java
+++ fridaymarket_auth/src/main/java/com/smile/fridaymarket_auth/domain/user/controller/TokenController.java
@@ -1,13 +1,12 @@
 package com.smile.fridaymarket_auth.domain.user.controller;
 
 import com.smile.fridaymarket_auth.domain.auth.token.service.RefreshTokenManager;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,7 +14,8 @@ public class TokenController {
 
     private final RefreshTokenManager refreshTokenManager;
 
-    @GetMapping("/api/refresh")
+    @Operation(summary = "accessToken 재발급", description = "accessToken 재발급 합니다.")
+    @RequestMapping(value = "/api/refresh", method = RequestMethod.GET)
     public ResponseEntity<HttpHeaders> refreshAccessToken(@RequestHeader("RefreshToken") String refreshToken) {
 
         HttpHeaders headers = refreshTokenManager.refreshAccessToken(refreshToken);
