b893d99b2ede771328ee69bff9c0c3cf2211a078
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-09-11 23:25:16 +0900

    [Refactor] 주문 등록 시 예외 처리

--- fridaymarket_resource/src/main/java/com/smile/fridaymarket_resource/global/exception/ErrorCode.java
+++ fridaymarket_resource/src/main/java/com/smile/fridaymarket_resource/global/exception/ErrorCode.java
@@ -6,7 +6,11 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
 
-    ;
+    // Token
+    TOKEN_NOT_VALID(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
+
+    // Product
+    PRODUCT_NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않는 상품입니다.");
     private final HttpStatus httpStatus;
     private final String message;
 
