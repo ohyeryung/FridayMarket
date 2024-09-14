448a5972e20b3e58d26526e599371048b9804282
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-09-14 22:12:32 +0900

    [Refactor] 주문 상태값 변경 예외 처리 추가

--- fridaymarket_resource/src/main/java/com/smile/fridaymarket_resource/global/exception/ErrorCode.java
+++ fridaymarket_resource/src/main/java/com/smile/fridaymarket_resource/global/exception/ErrorCode.java
@@ -7,10 +7,15 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     // Token
-    TOKEN_NOT_VALID(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
+    TOKEN_NOT_VALID(HttpStatus.BAD_REQUEST, "유효하지 않은 토큰입니다."),
 
     // Product
-    PRODUCT_NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않는 상품입니다.");
+    PRODUCT_NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않는 상품입니다."),
+
+    // Order
+    ORDER_NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않는 주문입니다."),
+    ILLEGAL_ORDER_USER_NOT_MATCHED(HttpStatus.BAD_REQUEST, "본인의 주문만 조회가 가능합니다.");
+
     private final HttpStatus httpStatus;
     private final String message;
 
